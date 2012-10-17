368dcbb4b1abc333ca72c1ff28de75f9cdae1063
陈赟 <cyhjl@sjtu.org>
2012-10-17 13:47:44 +0800

    1.1

--- 代码/EquipSearch/protected/views/site/index.php
+++ 代码/EquipSearch/protected/views/site/index.php
@@ -1,5 +1,6 @@
 <script>
 var type;
+var CONTENT;
 function search_friends(){
 	$("#listAll").html("正在查询，稍等。。。");
 	var player_name = $("#player_name").val();
@@ -9,11 +10,12 @@ function search_friends(){
 		url: 'index.php', 
 		dataType: 'json', 
 		data: data, 
-		success: function(content){	
+		success: function(content){
+				CONTENT = content;	
 				if(content){
 					str = '<tr><td>玩家名字</td><td>玩家id</td><td>装备id</td><td>装备等级</td><td>持有忍着id</td><td>color</td><td>是否删除</td><td>update_time</td><td>record_time</td></tr>';			
 					for(var key in content){
-	 					str+="<tr><td>"+content[key].name+"</td><td>"+content[key].player_id+"</td><td>"+content[key].equip_id+"</td><td>"+content[key].level+"</td><td>"+content[key].ninja_id+"</td><td>"+content[key].color+"</td><td>"+content[key].del_flag+"</td><td>"+content[key].update_time+"</td><td>"+content[key].record_time+"</td></tr>";
+	 					str+="<tr><td>"+content[key].name+"</td><td>"+content[key].player_id+"</td><td onclick='look_property("+key+")'>"+content[key].equip_id+"</td><td>"+content[key].level+"</td><td>"+content[key].ninja_id+"</td><td>"+content[key].color+"</td><td>"+content[key].del_flag+"</td><td>"+content[key].update_time+"</td><td>"+content[key].record_time+"</td></tr>";
 					}
 				}else{
 					str = "没有该玩家的装备数据";
@@ -30,6 +32,11 @@ function changeType(myitem){
 		type = 0;
 	 }
 }
+
+function look_property(key){
+	var info = [CONTENT[key].equip_id, CONTENT[key].color, CONTENT[key].property]; 
+	var obj=window.showModalDialog("property.php",info);
+}
 </script>
 
 <div class="bg_wrap_03 clearfix pb10">
@@ -43,7 +50,5 @@ function changeType(myitem){
 </div>
  
 
-<table id='listAll' width="100%" border="0" cellspacing="0" cellpadding="0" id="t3" >
-	 
-</table>
+<table id='listAll' width="100%" border="0" cellspacing="0" cellpadding="0" id="t3" ></table>
 	
