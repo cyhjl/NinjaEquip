d194abdeca42e305f9473ab5e5b369c5df136fbc
陈赟 <cyhjl@sjtu.org>
2012-10-17 17:59:36 +0800

    2.0

--- 代码/EquipSearch/property.php
+++
@@ -1,9 +0,0 @@
-<script>
-
-var info=window.dialogArguments;
-alert(info);
-//$("#AllProperty").html(property_str);
-
-</script>
-
-<table id='AllProperty' width="100%" border="0" cellspacing="0" cellpadding="0" id="t3" ></table>
--- 代码/EquipSearch/protected/controllers/SiteController.php
+++ 代码/EquipSearch/protected/controllers/SiteController.php
@@ -27,80 +27,8 @@ class SiteController extends Controller
 	 */
 	public function actionIndex()
 	{
-		// renders the view file 'protected/views/site/index.php'
-		// using the default layout 'protected/views/layouts/main.php'
 		$this->render('index');
 	}
-
-	/**
-	 * This is the action to handle external exceptions.
-	 */
-	public function actionError()
-	{
-	    if($error=Yii::app()->errorHandler->error)
-	    {
-	    	if(Yii::app()->request->isAjaxRequest)
-	    		echo $error['message'];
-	    	else
-	        	$this->render('error', $error);
-	    }
-	}
-
-	/**
-	 * Displays the contact page
-	 */
-	public function actionContact()
-	{
-		$model=new ContactForm;
-		if(isset($_POST['ContactForm']))
-		{
-			$model->attributes=$_POST['ContactForm'];
-			if($model->validate())
-			{
-				$headers="From: {$model->email}\r\nReply-To: {$model->email}";
-				mail(Yii::app()->params['adminEmail'],$model->subject,$model->body,$headers);
-				Yii::app()->user->setFlash('contact','Thank you for contacting us. We will respond to you as soon as possible.');
-				$this->refresh();
-			}
-		}
-		$this->render('contact',array('model'=>$model));
-	}
-
-	/**
-	 * Displays the login page
-	 */
-	public function actionLogin()
-	{
-		$model=new LoginForm;
-
-		// if it is ajax validation request
-		if(isset($_POST['ajax']) && $_POST['ajax']==='login-form')
-		{
-			echo CActiveForm::validate($model);
-			Yii::app()->end();
-		}
-
-		// collect user input data
-		if(isset($_POST['LoginForm']))
-		{
-			$model->attributes=$_POST['LoginForm'];
-			// validate user input and redirect to the previous page if valid
-			if($model->validate() && $model->login())
-				$this->redirect(Yii::app()->user->returnUrl);
-		}
-		// display the login form
-		$this->render('login',array('model'=>$model));
-	}
-
-	/**
-	 * Logs out the current user and redirect to homepage.
-	 */
-	public function actionLogout()
-	{
-		Yii::app()->user->logout();
-		$this->redirect(Yii::app()->homeUrl);
-	}
-
 	public function actionSearchFromEquipList(){
     	$session = new CHttpSession;
 	    $session->open();
@@ -108,17 +36,27 @@ class SiteController extends Controller
 	    $type = isset($_REQUEST['type'])?$_REQUEST['type']:0;
 	    $criteria = new EMongoCriteria;
 	    if($type==1){
-	    	$criteria->name('==',new MongoRegex("/.*".$player_name.".*/"))->limit(10);
+	    	$criteria->name('==',new MongoRegex("/.*".$player_name.".*/"));
 	    }else{
 	    	$criteria->name('==',$player_name);
 	    }
-
- 		$da = Equips::model()->findAll($criteria);
-		if(json_encode($da)!='[]'){
-			echo json_encode($da);
+  		$equips = Equips::model()->findAll($criteria);
+ 		if(json_encode($equips)!='[]'){
+			echo json_encode($equips);
 		}else{
 			echo null;
 		}
 		exit();
     }
+	public function actionGetEquipInfo(){
+    	$session = new CHttpSession;
+	    $session->open();
+	    $eid 		= isset($_REQUEST['eid'])?$_REQUEST['eid']:0;
+	    $color		= isset($_REQUEST['color'])?$_REQUEST['color']:0;
+ 	    $msEquipTem = new MSEquipTemplate($eid,$color);
+	    $res = array("img"=>$msEquipTem->getImgSrc(),"name"=>$msEquipTem->getEquipName(),"desc"=>$msEquipTem->getEquipDesc());
+ 		echo json_encode($res);
+ 		exit();
+    }
+
 }
